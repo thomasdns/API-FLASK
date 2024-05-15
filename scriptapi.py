5264ac5e927cfc5c0c36ee70835309a30afbe82c
thomasdns <dasneves.thomas.sio@gmail.com>
2024-05-15 14:52:04 +0200

    Regroupement de 2 api : pokemon et star wars

--- scriptapi.py
+++ scriptapi.py
@@ -10,26 +10,56 @@ def hello_world():
 def goodbye_world():
     return "Goodbye, World!"
 
-@app.route("/pokemon/<name>")
-def pokemon(name):
-    response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{name}")
-    if response.status_code == 200:
-        pokemon_data = response.json()
-        pokemon_name = pokemon_data['name']
-        pokemon_id = pokemon_data['id']
-        return f"Name: {pokemon_name}, ID: {pokemon_id}"
-    else:
-        return "Pokemon not found!"
+# @app.route("/pokemon/<name>")
+# def pokemon(name):
+#     response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{name}")
+#     if response.status_code == 200:
+#         pokemon_data = response.json()
+#         pokemon_name = pokemon_data['name']
+#         pokemon_id = pokemon_data['id']
+#         return f"Name: {pokemon_name}, ID: {pokemon_id}"
+#     else: 
+#         return "Pokemon not found!"
     
-@app.route("/stw/<id>")
-def starwars(id) :
-    response2 = requests.get(f"https://swapi.dev/api/people/{id}")
-    if response2.status_code == 200:
-        stw_data = response2.json()
-        stw_name = stw_data['name']
-        stw_mass = stw_data['mass']
-        return f"Name: {stw_name}, Mass: {stw_mass}"
-    elif response2.status_code == 404:
-        return "Character not found!"
- 
+# @app.route("/stw/<id>")
+# def starwars(id) :
+#     response2 = requests.get(f"https://swapi.dev/api/people/{id}")
+#     if response2.status_code == 200:
+#         stw_data = response2.json()
+#         stw_name = stw_data['name']
+#         stw_mass = stw_data['mass']
+#         return f"Name: {stw_name}, Mass: {stw_mass}"
+#     else :
+#         return "Character not found!"
+    # Autre façon de faire (=exeption)
+    # try :
+    #     stw_data = response2.json()
+    #     stw_name = stw_data['name']
+    #     stw_mass = stw_data['mass']
+    #   return True
+    # except :
+    #     return False
+
+
+@app.route("/find/<univers>/<name>")
+def find(univers,name):
+    if univers == "sw" :
+        response = requests.get(f"https://swapi.dev/api/people/{name}/")
+        if response.status_code == 200:
+            sw_data = response.json()
+            sw_name = sw_data['name']
+            return f"Name: {sw_name}"
+        else :
+            return "Character not found!"
+    elif univers == "pk":
+        response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{name}")
+        if response.status_code == 200:
+            pk_data = response.json()
+            pk_name = pk_data['name']
+            return f"Name: {pk_name}"
+        else :
+            return "Pokemon not found!"
+    else :
+        return "univers non reconnu !"
+
 app.run()
